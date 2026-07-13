c0aa232253ad41531ee61094bd63671259af257e
Darshil Rakesh Shah <68950715+shahdarshil123@users.noreply.github.com>
2026-07-13 08:43:03 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Optimized solution
+        nums_map = {}
+        result = []
+        for i in range(len(nums)):
+            if target - nums[i] in nums_map:
+                result = [nums_map[target-nums[i]], i]
+                return result
+            nums_map[nums[i]] = i
+        return result
+        
